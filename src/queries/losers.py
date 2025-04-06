d41ab3d4ec0abb4dbb579b201500589f82792bf1
PudgeKim <zxaq15@gmail.com>
2025-04-06 12:43:24 +0900

    최근 5일 꼴찌 가져오는 쿼리 추가

--- src/queries/losers.py
+++ src/queries/losers.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import func
+from sqlalchemy import func, desc
 from sqlalchemy.orm import Session
 
 from src import models
-from src.utils import get_db, get_today_range
+from src.utils import get_db, get_today_range, get_recent_days_range
 
 router = APIRouter()
 
 
 @router.get("/today-losers")
 def get_today_losers(db: Session = Depends(get_db)):
-    today_start, tomorrow_start = get_today_range()
+    start_time, end_time = get_today_range()
 
     subquery = (
         db.query(
@@ -19,8 +19,8 @@ def get_today_losers(db: Session = Depends(get_db)):
             func.sum(models.RaceResult.rank).label("total_rank")
         )
             .join(models.RaceResult, models.Race.id == models.RaceResult.race_id)
-            .filter(models.Race.created_at >= today_start)
-            .filter(models.Race.created_at < tomorrow_start)
+            .filter(models.Race.created_at >= start_time)
+            .filter(models.Race.created_at < end_time)
             .group_by(models.Race.group_uuid, models.RaceResult.character_name)
             .subquery()
     )
@@ -50,6 +50,50 @@ def get_today_losers(db: Session = Depends(get_db)):
             "group_uuid": row.group_uuid,
             "character_name": row.character_name,
             "total_rank": row.total_rank
+        }
+        for row in losers.values()
+    ]
+
+
+@router.get("/recent-losers")
+def get_recent_losers(db: Session = Depends(get_db)):
+    start_time, end_time = get_recent_days_range(5)
+
+    subquery = (
+        db.query(
+            models.Race.group_uuid,
+            models.RaceResult.character_name,
+            models.Race.created_at,
+            func.sum(models.RaceResult.rank).label("total_rank")
+        )
+            .join(models.RaceResult, models.Race.id == models.RaceResult.race_id)
+            .filter(models.Race.created_at >= start_time)
+            .filter(models.Race.created_at < end_time)
+            .group_by(models.Race.group_uuid, models.RaceResult.character_name)
+            .order_by(desc(models.Race.created_at))
+            .subquery()
+    )
+
+    rows = db.query(
+        subquery.c.group_uuid,
+        subquery.c.character_name,
+        subquery.c.created_at,
+        subquery.c.total_rank
+    ).all()
+
+    losers = {}
+    for row in rows:
+        group = row.group_uuid
+        current = losers.get(group)
+        if not current or row.total_rank > current.total_rank:
+            losers[group] = row
+
+    return [
+        {
+            "group_uuid": row.group_uuid,
+            "character_name": row.character_name,
+            "total_rank": row.total_rank,
+            "created_at": row.created_at
         }
         for row in losers.values()
     ]
